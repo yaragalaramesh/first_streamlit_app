39fccece15943b0b8b9ade9d3f17b8cbc792ac5a
yaragalaramesh <133313902+yaragalaramesh@users.noreply.github.com>
2023-05-16 16:38:19 +0530

    Update streamlit_app.py
    added another text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', fruit_choice)
