c7f275041aba72f4bca7f18b32891e7b50d9de9d
yaragalaramesh <133313902+yaragalaramesh@users.noreply.github.com>
2023-05-16 08:29:40 +0530

    Update streamlit_app.py
    Added some interactivity to pick the fruits customer needed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
-import pandas
 
-streamlit.title('My parents New Healthy diner')
+streamlit.title('My Moms New Healthy diner')
 streamlit.header('Breakfast favorites')
 streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
@@ -10,7 +9,13 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
 
