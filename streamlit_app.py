0121a0f265cab37ef93d9784426f0d691c0be638
yaragalaramesh <133313902+yaragalaramesh@users.noreply.github.com>
2023-05-16 18:12:04 +0530

    Update streamlit_app.py
    made defailt insert fruit to dynamic

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,18 +53,20 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cx.close()
    streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add the fruit to the list 
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit') ")
+      my_cur.execute("insert into FRUIT_LOAD_LIST values(' " + new_fruit + "') ")
       return "Thanks for adding" + new_fruit
    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list:'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
+   my_cx.close()
    streamlit.text(back_from_function)
    
 # streamlit.write('The for adding ', fruit_choice)
