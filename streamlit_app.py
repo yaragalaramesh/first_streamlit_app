8213e79bbfc4a129330ac848af5be3d2f4b960b7
yaragalaramesh <133313902+yaragalaramesh@users.noreply.github.com>
2023-05-16 16:28:20 +0530

    Update streamlit_app.py
    querying all data instead one row

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
