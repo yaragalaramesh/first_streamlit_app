bc7b53ee1d4f8d7e929eae85cc928a485cdba6d6
yaragalaramesh <133313902+yaragalaramesh@users.noreply.github.com>
2023-05-16 17:07:34 +0530

    Update streamlit_app.py
    introduced try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,20 +23,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#new section to display fruityVice data
+#new section to display fruityVice choice
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# take the json response and make it  normalized(table) data rather than static 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# print the normalized data to the screen
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      stremlit.error('Please select a fruit to get information')
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we trouble shoot
 streamlit.stop()
